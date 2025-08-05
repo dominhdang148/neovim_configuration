ce61f7089589c030bafaa2a15b5c526bc7ef180e
dominhdang148 <myclone235@gmail.com>
2025-08-05 09:43:09 +0700

    Finished initial init

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "eldritch-theme/eldritch.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "eldritch",
+    },
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = { "java", "html", "css", "typescript", "lua", "tsx", "javascript" },
+    auto_install = true,
+    hightlight = {
+      enable = true,
+    },
+  },
+}
