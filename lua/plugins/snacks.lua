4ce7fcddf4af592517409cfdf7e6972b9435510e
dominhdang148 <myclone235@gmail.com>
2025-12-06 11:44:29 +0700

    ready to update

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,7 @@
 return {
   "snacks.nvim",
   opts = {
+    lazygit = {},
     indent = { enabled = false },
     exclude = {
       ".git",
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,24 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "java", "html", "css", "typescript", "lua", "tsx", "javascript", "python", "ninja", "rst" },
+    ensure_installed = {
+      "java",
+      "html",
+      "css",
+      "typescript",
+      "lua",
+      "tsx",
+      "javascript",
+      "python",
+      "ninja",
+      "rst",
+      "git_config",
+      "gitcommit",
+      "git_rebase",
+      "gitignore",
+      "gitattributes",
+      "yaml",
+    },
     auto_install = true,
     hightlight = {
       enable = true,
